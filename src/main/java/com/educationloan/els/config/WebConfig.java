49b55f880f440d2b8d6f4e9487453f07eda8beda
Sanket Darunkar <sanketdarunkar111@gmail.com>
2025-12-23 23:51:17 +0530

    Add base dto, config, and util packages

---
+++ src/main/java/com/educationloan/els/config/WebConfig.java
@@ -0,0 +1,8 @@
+package com.educationloan.els.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+    // MVC / View resolver configs will go here
+}
---
+++ src/main/java/com/educationloan/els/dto/StudentDto.java
@@ -0,0 +1,5 @@
+package com.educationloan.els.dto;
+
+public class StudentDto {
+    // will be extended later
+}
