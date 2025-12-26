af597732c0d6e781d2100deb1da3b989ffe3befe
Rajnish Singh <103492286+rajnish0504@users.noreply.github.com>
2025-12-27 01:06:45 +0530

    Update RajnishTestController.java
    testing from rajnish

--- src/main/java/com/els/educationloansystem/controller/RajnishTestController.java
+++ src/main/java/com/els/educationloansystem/controller/RajnishTestController.java
@@ -1,5 +1,12 @@
 package com.els.educationloansystem.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/rajnish")
 public class RajnishTestController {
     
+    
+
 }
