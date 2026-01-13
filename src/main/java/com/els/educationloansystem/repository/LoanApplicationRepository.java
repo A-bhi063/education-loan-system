5528e49680523c23f683da034be07ee1484ae5bd
Rajnish Singh <103492286+rajnish0504@users.noreply.github.com>
2026-01-14 00:10:56 +0530

    Admin Apis added and JWT Admin Student Authorization

--- src/main/java/com/els/educationloansystem/repository/LoanApplicationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.els.educationloansystem.repository;
-
-import com.els.educationloansystem.entity.LoanApplication;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface LoanApplicationRepository extends JpaRepository<LoanApplication, Long> {
-
-}
